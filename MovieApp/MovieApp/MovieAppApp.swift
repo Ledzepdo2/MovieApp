f662d1c4692e12023844677944a51867145f7613
Jesus Perez <ios.developer.jesus@gmail.com>
2025-09-30 22:56:15 -0600

    Initializes the MovieApp project
    Sets up the basic project structure for the MovieApp,
    including project files, assets, and initial SwiftUI views.
    Also configures unit and UI testing targets.
    
    Removes unused file from project
    
    Removes a reference to a non-existent file and
    replaces it with a reference to the correct UITests file in the project configuration.

---
+++ MovieApp/MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Jesus Perez on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
